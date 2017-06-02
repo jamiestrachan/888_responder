6b4a1f5f0f1d90dbb27c6dddced0e907ef5ad79f
Jamie Strachan <jstrachan@infotech.com>
2017-06-01 23:46:10 -0400

    adding response for photos

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var twit = require('twit');
 var config = require('./config.js');
+const fs = require('fs');
 //var server = require('http').createServer();
 //var io = require('socket.io')(server);
 var mysql      = require('mysql');
@@ -14,6 +15,13 @@ var T = new twit(config);
 
 var stream = T.stream('user');
 
+function prettyPrintDate(date_string) {
+  var date_object = new Date(date_string);
+  var formatted_date = date_object.toDateString();
+  var date_pieces = formatted_date.match(/[A-z]+ ([A-z]+) ([0-9]+) ([0-9]+)/);
+  return date_pieces[1] + " " + date_pieces[2] + ", " + date_pieces[3];
+}
+
 stream.on('tweet', tweetEvent);
 
 // Here a tweet event is triggered!
@@ -27,6 +35,8 @@ function tweetEvent(tweet) {
 
   // Who is this in reply to?
   var reply_to = tweet.in_reply_to_screen_name;
+  var original_tweet_id = tweet.id;
+  console.log(original_tweet_id);
   // Who sent the tweet?
   var name = tweet.user.screen_name;
   // What is the text?
@@ -38,28 +48,77 @@ function tweetEvent(tweet) {
     // Get rid of the @ mention
     txt = txt.replace(/@888yongeandold/g,'');
 
-    var match_results = txt.match(/When was the last time ([^;'"=]+) played there\?/i);
-    //console.log(match_results);
-    if (match_results) {
+    var lasttime_results = txt.match(/When was the last time ([^;'"=]+) played there\?/i);
+    //console.log(lasttime_results);
+    if (lasttime_results) {
       //connection.connect();
 
       connection.query(
-        'SELECT * from Concerts WHERE headliner LIKE "%' + match_results[1] + '%" ORDER BY date DESC',
+        'SELECT * from Concerts WHERE headliner LIKE "%' + lasttime_results[1] + '%" ORDER BY date DESC LIMIT 1',
         function (error, results, fields) {
           if (error) throw error;
-          var previous_date = new Date(results[0].date);
-          var formatted_date = previous_date.toDateString();
-          var date_pieces = formatted_date.match(/[A-z]+ ([A-z]+) ([0-9]+) ([0-9]+)/);
           var reply = '.@'+name + ' ';
-          reply += 'The last time ' + match_results[1] + ' played here was at ' + results[0].venue_name + ' on ' + date_pieces[1] + " " + date_pieces[2] + ", " + date_pieces[3];
-          T.post('statuses/update', { status: reply }, tweeted);
+          if (results.length > 0) {
+            reply += results[0].headliner + ' played here at ' + results[0].venue_name + ' on ' + prettyPrintDate(results[0].date);
+          } else {
+            reply += "Sorry, I don't when " + lasttime_results[1] + " last played here. Maybe check https://en.wikipedia.org/wiki/Masonic_Temple_(Toronto)";
+          }
+
+          T.post('statuses/update', { status: reply, in_reply_to_status_id: original_tweet_id }, tweeted);
+        }
+      );
+    }
+
+    var pictures_results = txt.match(/Do you have any pictures of ([^;'"=]+)\?/i);
+    //console.log(pictures_results);
+    if (pictures_results) {
+      //connection.connect();
+
+      connection.query(
+        'SELECT * from Concerts WHERE headliner LIKE "%' + pictures_results[1] + '%" AND local_image IS NOT NULL ORDER BY rand() LIMIT 1',
+        function (error, results, fields) {
+          if (error) throw error;
+          var reply = '.@'+name + ' ';
+          if (results.length > 0) {
+            var b64content = fs.readFileSync(results[0].local_image, { encoding: 'base64' })
+
+            // first we must post the media to Twitter
+            T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+              // now we can assign alt text to the media, for use by screen readers and
+              // other text-based presentations and interpreters
+              var mediaIdStr = data.media_id_string
+              var altText = pictures_results[1] + " at " + results[0].venue_name;
+              var meta_params = { media_id: mediaIdStr, alt_text: { text: altText } }
+
+              T.post('media/metadata/create', meta_params, function (err, data, response) {
+                if (!err) {
+                  // now we can reference the media and post a tweet (media will attach to the tweet)
+                  var params = {
+                    status: reply + "I do! Here's " + pictures_results[1] + " at " + results[0].venue_name + " on " + prettyPrintDate(results[0].date),
+                    in_reply_to_status_id: original_tweet_id,
+                    media_ids: [mediaIdStr]
+                  }
+                  console.log(params);
+
+                  T.post('statuses/update', params, tweeted);
+                }
+              })
+            })
+
+          } else {
+            reply += "No I don't. Sorry :(";
+            T.post('statuses/update', { status: reply }, tweeted);
+          }
+
         }
       );
 
-      //connection.end();
+    //connection.end();
 
-    }
   }
+
+}
+
 
 
 
